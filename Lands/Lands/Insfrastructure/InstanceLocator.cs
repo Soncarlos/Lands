bdb1dc3b5d7ddf8e511040a350000469061b56c3
Carlos Jose Espinoza <charlie8219@outlook.com>
2018-11-24 23:52:37 -0600

    Login Page

---
+++ Lands/Lands/Insfrastructure/InstanceLocator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Insfrastructure
+{
+    using ViewModels;
+    
+    public class InstanceLocator
+    {
+        #region Property
+        public MainViewModels Main { get; set; }
+        #endregion
+
+        #region constructors
+
+        public InstanceLocator()
+        {
+          this.Main = new MainViewModels();
+
+        }
+
+        #endregion
+    }
+}
