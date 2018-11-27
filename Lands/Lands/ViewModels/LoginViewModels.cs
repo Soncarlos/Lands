462657fe65688340e9bc8c8af7ee2a22e1b4f494
Soncarlos <soncarlos1982@gmail.com>
2018-11-26 23:37:09 -0600

    BindingObjects LoginViewModel

---
+++ Lands/Lands/ViewModels/LoginViewModels.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+   public class LoginViewModels
+    {
+         
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunnig { get; set; }
+        public bool IsRemenbered { get; set; }
+        #endregion
+
+        #region costructors
+        public LoginViewModels()
+        {
+            this.IsRemenbered = true;
+
+        }
+        #endregion
+
+        #region Commands
+
+        public ICommand LoginCommand { get; set; }     
+
+        #endregion
+   }
+
+    
+}
--- Lands/Lands/ViewModels/MainViewModels.cs
+++ Lands/Lands/ViewModels/MainViewModels.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Lands.ViewModels
 {
+    using ViewModels;
      public class MainViewModels
     {
+        #region properties
+        public LoginViewModels Login
+        { get; set; }
+        #endregion
+
+        #region constructor
+        public MainViewModels()
+        {
+            this.Login = new LoginViewModels();
+
+        }
+        #endregion
+
     }
 }
