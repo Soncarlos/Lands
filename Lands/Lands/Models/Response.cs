7715cbe5d9867ed4d3f6324a7eac35cfa2e56a51
Soncarlos <soncarlos1982@gmail.com>
2018-12-02 16:38:03 -0600

    Se consume el Sevicio de Paises
    Se crea el apiservice que nos permite cargar nuestro servicio desde la LandviewModels

---
+++ Lands/Lands/Models/Response.cs
@@ -0,0 +1,27 @@
+﻿namespace Lands.Models
+{
+    //CLASE QUE NOS AYUDARA A COMSUMIR EL SERVICIO
+  public class Response
+    {
+        //
+        public bool IsSuccess
+        {
+            get;
+            set;
+                       
+        }
+        //
+        public string Message
+        { get;
+          set;
+        }
+
+        //
+        public object Result
+        {
+          get;
+          set;
+        }
+    }
+
+}
--- Lands/Lands/ViewModels/LandsViewModels.cs
+++ Lands/Lands/ViewModels/LandsViewModels.cs
@@ -1,7 +1,67 @@
-﻿
-namespace Lands.ViewModels
+﻿namespace Lands.ViewModels
 {
-    public class LandsViewModels
+    using Lands.Models;
+    using System.Collections.ObjectModel;
+    using Services;
+    using Xamarin.Forms;
+    using System.Collections.Generic;
+
+    public class LandsViewModels: BaseViewModels
     {
+
+        #region Services
+        private ApiService apiServices;
+
+        #endregion
+        #region Attibutes
+        // se crea un observablecolecction porque se va a pintar en un List view
+        private ObservableCollection<Land> lands;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land> Lands
+        {
+            get {return lands;}
+            set {SetValue(ref lands,  value );}
+
+        }
+        #endregion
+
+        #region Constructor
+        public LandsViewModels()
+        {
+            apiServices = new ApiService();
+            LoadLands();
+        }
+        #endregion
+
+        #region Methods
+        // 
+        private async void LoadLands()
+        {
+
+            var response = await apiServices.GetList<Land>(
+                "http://www.restcountries.eu",
+                "/rest",
+                "/v2/all");
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                 "Error", response.Message, "Accept");
+                return;
+               
+            }
+            // Se castea la lista resultante al modelo Land
+            var Mylist = (List<Land>)response.Result;
+
+            Lands = new ObservableCollection<Land>(Mylist);
+
+
+
+        }
+        #endregion
+
+        
     }
 }
